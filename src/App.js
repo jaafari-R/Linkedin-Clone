d7b8bf65a2a9170c5f15c8c1f94ba0302fd6e24f
potato <jaafariramadan@gmail.com>
2023-06-04 20:37:22 +0300

    init header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
   return (
     <div className="App">
-      <h1>LinkedIn Clone</h1>
+      {/* Header */}
+      <Header></Header>
+
+      {/* App Body */}
+
+      {/* SideBar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1> Header </h1>
+
+      <div className="header__left">
+        <img src="" alt="" />
+
+        <div className="header__search">
+          {/* SearchIcon */}
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
