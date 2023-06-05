fe5c4a238c2bbfd29de381541f9c9881d3811222
potato <jaafariramadan@gmail.com>
2023-06-05 08:04:26 +0300

    Header Done

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,13 @@ import React from 'react'
 import './Header.css'
 
 import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+import HeaderOption from './HeaderOption';
 
 function Header() {
   return (
@@ -11,12 +18,19 @@ function Header() {
 
         <div className="header__search">
           <SearchIcon />
-          <input type="text" />
+          <input type="text" placeholder='Search'/>
         </div>
       </div>
 
       <div className="header__right">
-        
+        <HeaderOption Icon={HomeIcon} title='Home'/>
+        <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+        <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+        <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+        <HeaderOption Icon={ChatIcon} title='Messages'/>
+        <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+
+        <HeaderOption avatar='/linkedin.png' title='me'/>
       </div>
     </div>
   )
