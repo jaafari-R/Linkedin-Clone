f004e35f6402385921be5eb8dd96af176cdd110c
potato <jaafariramadan@gmail.com>
2023-06-06 20:39:27 +0300

    Feed Done

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './Post.css';
 
 import { Avatar } from '@mui/material';
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
+import MessageOutlinedIcon from '@mui/icons-material/MessageOutlined';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import SendIcon from '@mui/icons-material/Send';
+
+import InputOptions from './InputOptions';
 
 function Post({ name, description, message, photoUrl }) {
   return (
@@ -13,8 +19,16 @@ function Post({ name, description, message, photoUrl }) {
           <p>{description}</p>
         </div>
       </div>
+      
       <div className="post__body">
         <p>{message}</p>
+      </div>
+
+      <div className="post__buttons">
+        <InputOptions Icon={ThumbUpAltOutlinedIcon} title='Like' />
+        <InputOptions Icon={MessageOutlinedIcon} title='Comment' />
+        <InputOptions Icon={RepeatIcon} title='Repost' />
+        <InputOptions Icon={SendIcon} title='Send' />
       </div>
     </div>
   )
