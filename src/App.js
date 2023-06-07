1bbf83944eb9f596d3737dc9da7ef7fb0d61a09e
potato <jaafariramadan@gmail.com>
2023-06-07 22:52:58 +0300

    Added userSlice with Redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import './App.css';
 
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widgets from './Widgets';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      {/* Header */}
-      <Header></Header>
+      {!user ? (<login />)
+          : (
+            <div>
+              <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+              <div className="app__body">
+                <Sidebar />
+                <Feed />
+                <Widgets />
+              </div>
+            </div>
+          )
+      }
     </div>
   );
 }
